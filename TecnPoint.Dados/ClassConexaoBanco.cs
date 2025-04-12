a13ef1773c5d1aff789e36b3862e778908dc01c2
samuel <samuelconnde@gmail.com>
2025-04-12 18:45:36 -0300

    Desenvolvimento da classe para conexão com o banco de dados.

---
+++ TecnPoint.Dados/ClassConexaoBanco.cs
@@ -0,0 +1,25 @@
+﻿using Npgsql;
+
+namespace TecnPoint.Dados
+{
+    public class ClassConexaoBanco : IDisposable
+    {
+
+        private readonly string _stringconexao;
+        public NpgsqlConnection conexao { get; set; } //Representa a conexao aberta com o banco
+
+        public ClassConexaoBanco()
+        {
+            _stringconexao = "Host=localhost;Port=5432;Username=postgres;Password=ADMIN;Database=SystemDB";
+            conexao = new NpgsqlConnection(_stringconexao);
+            conexao.Open();
+
+        }
+
+        public void Dispose()
+        {
+            conexao.Dispose();
+        }
+
+    }
+}
