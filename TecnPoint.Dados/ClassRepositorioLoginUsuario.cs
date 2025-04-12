d32f62e6b16bb527321c5ac9881e417d3d2c9c21
samuel <samuelconnde@gmail.com>
2025-04-12 20:13:12 -0300

    Atualizando classe de repositório do usuário.

---
+++ TecnPoint.Dados/ClassRepositorioLoginUsuario.cs
@@ -0,0 +1,49 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TecnPoint.Modelo.ClassLoginUsuario;
+
+namespace TecnPoint.Dados
+{
+    public class ClassRepositorioLoginUsuario
+    {
+        
+        //Método para obter os dados do usuário para validar login
+        public ClassLoginUsuario ConsultaEmailSenha(string emailConsultado, string senhaConsultada)
+        {
+            using (ClassConexaoBanco conexaoConsultaEmailSenha = new ClassConexaoBanco())
+            {
+                string query = "SELECT Email, Senha, tipo_Usuario FROM Usuarios WHERE Email = @RecebeEmail AND Senha = @RecebeSenha";
+
+                using (NpgsqlCommand comando = new NpgsqlCommand(query, conexaoConsultaEmailSenha.conexao))
+                {
+                    //Enviando parâmetros para utilizar na consulta
+                    comando.Parameters.AddWithValue("@RecebeEmail", emailConsultado);
+                    comando.Parameters.AddWithValue("@RecebeSenha", senhaConsultada);
+
+                    using (NpgsqlDataReader leitorDeColunas = comando.ExecuteReader())
+                    {
+                        if (leitorDeColunas.Read()) //Se encontrou algum dado
+                        {
+                            if ((String.Equals(leitorDeColunas.GetString(leitorDeColunas.GetOrdinal("Email")), emailConsultado)) && (String.Equals(leitorDeColunas.GetString(leitorDeColunas.GetOrdinal("Senha")), senhaConsultada)))
+                            {
+                                return new ClassLoginUsuario(email:"", senha:"", tipoUsuario:"")
+                                {
+                                    //GetOrdinal vai retornar o número da coluna com base no seu nome
+                                    Email = leitorDeColunas.GetString(leitorDeColunas.GetOrdinal("Email")),
+                                    Senha = leitorDeColunas.GetString(leitorDeColunas.GetOrdinal("Senha")),
+                                    TipoUsuario = leitorDeColunas.GetString(leitorDeColunas.GetOrdinal("tipo_Usuario"))
+                                };
+                            }
+                        }
+                    }
+
+                }
+            }
+            return null; //Se o usuário não for encontrado
+        }
+    }
+}
