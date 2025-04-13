500157da05ede30692d66858cdd249313f1224d9
samuel <samuelconnde@gmail.com>
2025-04-12 21:43:37 -0300

    Adicionando classes de dado e modelo pronto para login de usuários após teste no Form1.

--- TecnPoint/Form1.Designer.cs
+++ TecnPoint/Form1.Designer.cs
@@ -30,54 +30,19 @@ namespace TecnPoint
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            email = new TextBox();
-            senha = new TextBox();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(310, 221);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // email
-            // 
-            email.Location = new Point(263, 101);
-            email.Name = "email";
-            email.Size = new Size(179, 23);
-            email.TabIndex = 1;
-            // 
-            // senha
-            // 
-            senha.Location = new Point(263, 151);
-            senha.Name = "senha";
-            senha.Size = new Size(179, 23);
-            senha.TabIndex = 2;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(700, 338);
-            Controls.Add(senha);
-            Controls.Add(email);
-            Controls.Add(button1);
             Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Button button1;
-        private TextBox email;
-        private TextBox senha;
     }
 }
--- TecnPoint/Form1.cs
+++ TecnPoint/Form1.cs
@@ -1,38 +1,10 @@
-using TecnPoint.Dados;
-using TecnPoint.Modelo.ClassLoginUsuario;
-using TecnPoint.Modelo.ClassStaticUsuarioLogado;
-
 namespace TecnPoint
 {
     public partial class Form1 : Form
     {
-
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            //Criando objeto para passar os dados para o usuário
-            ClassRepositorioLoginUsuario dadosUsuarioLogado = new ClassRepositorioLoginUsuario();
-            ClassLoginUsuario usuarioUsuarioLogado = new ClassLoginUsuario(idUsuario: 0, "", "", "", "");
-            usuarioUsuarioLogado = dadosUsuarioLogado.ConsultaEmailSenha(email.Text, senha.Text);
-
-            if(usuarioUsuarioLogado != null)
-            {
-                MessageBox.Show("Login válido",
-                                "Tecn Solutions",
-                                MessageBoxButtons.OK,
-                                MessageBoxIcon.Information);
-            }
-            else
-            {
-                MessageBox.Show("Login inválido",
-                                "Tecn Solutions",
-                                MessageBoxButtons.OK,
-                                MessageBoxIcon.Error);
-            }
         }
     }
 }
