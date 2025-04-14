f452ce2fb3ef2162ede009bc8a09f2e86ffa9ca3
Pedro <pedronicolas576@gmail.com>
2025-04-14 12:47:30 -0300

    Incluindo nova tela de funcionario e estilizando os botões de criar chamado e acompanhar chamado

--- TecnPoint/FormTelaFuncionario.Designer.cs
+++ TecnPoint/FormTelaFuncionario.Designer.cs
@@ -1,16 +1,14 @@
-﻿using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
-
-namespace TecnPoint
+﻿namespace TecnPoint.Interface
 {
     partial class FormTelaFuncionario
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -25,24 +23,109 @@ namespace TecnPoint
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTelaFuncionario));
+            NomeUsuario = new Label();
+            EmailUsuario = new Label();
+            button2 = new Button();
+            panel1 = new Panel();
+            groupBox1 = new GroupBox();
             SuspendLayout();
             // 
-            // Form1
+            // NomeUsuario
+            // 
+            NomeUsuario.AutoSize = true;
+            NomeUsuario.BackColor = Color.Transparent;
+            NomeUsuario.ForeColor = Color.Gainsboro;
+            NomeUsuario.Location = new Point(75, 22);
+            NomeUsuario.Name = "NomeUsuario";
+            NomeUsuario.Size = new Size(83, 15);
+            NomeUsuario.TabIndex = 0;
+            NomeUsuario.Text = "Nome Usuario";
+            // 
+            // EmailUsuario
+            // 
+            EmailUsuario.AutoSize = true;
+            EmailUsuario.BackColor = Color.Transparent;
+            EmailUsuario.ForeColor = Color.Gainsboro;
+            EmailUsuario.Location = new Point(75, 47);
+            EmailUsuario.Name = "EmailUsuario";
+            EmailUsuario.Size = new Size(79, 15);
+            EmailUsuario.TabIndex = 1;
+            EmailUsuario.Text = "Email Usuario";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.Cursor = Cursors.Hand;
+            button2.FlatAppearance.BorderColor = Color.DarkGray;
+            button2.FlatAppearance.MouseDownBackColor = Color.FromArgb(190, 137, 254);
+            button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(163, 89, 253);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.Gainsboro;
+            button2.Image = Properties.Resources.ChecklistIconActive;
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(10, 111);
+            button2.Name = "button2";
+            button2.Size = new Size(183, 40);
+            button2.TabIndex = 3;
+            button2.Text = "Acompanhar um chamado";
+            button2.TextAlign = ContentAlignment.MiddleRight;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlLightLight;
+            panel1.Location = new Point(201, -5);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(504, 349);
+            panel1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.Transparent;
+            groupBox1.BackgroundImage = Properties.Resources.UserPicture;
+            groupBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            groupBox1.FlatStyle = FlatStyle.Flat;
+            groupBox1.Location = new Point(4, 5);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(70, 79);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            // 
+            // FormTelaFuncionario
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(100, 52, 144);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(700, 338);
-            Margin = new Padding(3, 2, 3, 2);
+            Controls.Add(button2);
+            Controls.Add(groupBox1);
+            Controls.Add(panel1);
+            Controls.Add(EmailUsuario);
+            Controls.Add(NomeUsuario);
+            DoubleBuffered = true;
             Name = "FormTelaFuncionario";
             Text = "FormTelaFuncionario";
+            Closed += FormTelaCliente_Closed;
+            Load += FormTelaCliente_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label NomeUsuario;
+        private Label EmailUsuario;
+        private Button button2;
+        private Panel panel1;
+        private GroupBox groupBox1;
     }
 }
--- TecnPoint/FormTelaFuncionario.cs
+++ TecnPoint/FormTelaFuncionario.cs
@@ -1,4 +1,16 @@
-namespace TecnPoint
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TecnPoint.Modelo.ClassStaticUsuarioLogado;
+
+
+namespace TecnPoint.Interface
 {
     public partial class FormTelaFuncionario : Form
     {
@@ -6,5 +18,26 @@ namespace TecnPoint
         {
             InitializeComponent();
         }
+       /* private void AlternarBotoes(Button ativo, Button inativo)
+        {
+            ativo.BackColor = Color.FromArgb(163, 89, 253);
+            inativo.BackColor = Color.Transparent;
+        }*/
+        private void FormTelaCliente_Closed(object sender, EventArgs e)
+        {
+            FormTelaLogin telaLogin = new FormTelaLogin();
+            telaLogin.Show();
+        }
+        private void FormTelaCliente_Load(object sender, EventArgs e)
+        {
+            NomeUsuario.Text = ClassStaticUsuarioLogado.Nome;
+            EmailUsuario.Text = ClassStaticUsuarioLogado.Email;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.FromArgb(163, 89, 253);
+
+        }
     }
+        
 }
