867f9778bf37c86e840f7e1f9934b2e022f778d5
Pedro <pedronicolas576@gmail.com>
2025-04-19 15:51:45 -0300

    Adicionando Class para Acompanhar chamado do Usuario

---
+++ TecnPoint.Modelo/DadosAcompanharChamado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecnPoint.Modelo.DadosAcompanharChamado;
+
+internal class DadosAcompanharChamado
+{
+    
+}
