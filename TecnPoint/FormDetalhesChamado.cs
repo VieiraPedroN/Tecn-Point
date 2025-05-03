35d288b88236de28ac62ff42921b91076fe490ad
Pedro <pedronicolas576@gmail.com>
2025-05-03 15:20:51 -0300

    Revert "Subido teste"
    This reverts commit 09ebc1f00c9cd406f6ec5c58ecdc991743485012.

--- TecnPoint/FormDetalhesChamado.cs
+++ TecnPoint/FormDetalhesChamado.cs
@@ -60,7 +60,7 @@ namespace TecnPoint.Interface
             ServCarregaPrioridade.CarregaPrioridade(cbxPrioridade);
             CarregaMensagem();
             carregandoCombo = false;
-            MessageBox.Show("teste");
+
         }
 
         private void button1_Click(object sender, EventArgs e)
